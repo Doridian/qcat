1d63dbf0d7ab847016ed09211f326d72e0b4f1b0
bdillo <ben.g.dillon@pm.me>
2024-08-25 10:00:47 -0700

    add actual error when parsing input salt/password rather than panicking, change QcatPassword struct name to SaltedPassword

--- src/utils.rs
+++ src/utils.rs
@@ -1,20 +1,19 @@
-use std::{error::Error, str::FromStr};
+use std::{self, str::FromStr};
 
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, BufReader};
 
-use crate::crypto::QcatPassword;
+use crate::crypto::SaltedPassword;
 
 /// Receive a password input by the user. Intended for use by the client with the generated server password
 pub async fn receive_password_input<T: AsyncReadExt + Unpin>(
     input: &mut T,
-) -> Result<QcatPassword, Box<dyn Error>> {
-    eprintln!("enter password:");
+) -> Result<SaltedPassword, Box<dyn std::error::Error>> {
+    eprintln!("Enter password from server:");
     let mut reader = BufReader::new(input);
     let mut received_password = String::new();
 
     reader.read_line(&mut received_password).await?;
-    // TODO: clean up this ownership?
     let received_password = received_password.trim();
 
-    QcatPassword::from_str(received_password)
+    Ok(SaltedPassword::from_str(received_password)?)
 }
